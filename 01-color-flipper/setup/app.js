9d6fe5dc13ec8cc227d2591c5d8f5d649eb1e45c
suitupMT <suitup.mt@gmail.com>
2023-01-06 04:25:03 -0600

    finished

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,23 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    //console.log(document.body);
+
+    //get random number between 0 and 3;
+    const randomNumber = getRandomNumber();
+    //console.log(randomNumber);
+
+    //targets the entire body of the document
+    document.body.style.backgroundColor = colors[randomNumber];
+
+    //changes the text of the color class
+    color.textContent = colors[randomNumber];
+    document.getElementById("name").style.color = colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+//example of hex color #F15025
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    //console.log(document.body);
+
+    //generate random Hex;
+    let randomHex = '#';
+    for (let i = 0; i < 6; i++) {
+        randomHex += hex[getRandomNumber()];
+    }
+
+    //targets the entire body of the document
+    document.body.style.backgroundColor = randomHex;
+
+    //changes the text of the color class
+    color.textContent = randomHex;
+    document.getElementById("name").style.color = randomHex;
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
